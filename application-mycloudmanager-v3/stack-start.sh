323c8deea72b404cb76a49178a2f8337623af5c8
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-16 15:21:42 +0100

    New HA Stack

--- application-mycloudmanager-v3/stack-start.sh
+++ application-mycloudmanager-v3/stack-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "Blueprint Kubernetes HA"
+echo "MyCloudManager V3"
 echo "-----------------------"
 if [[ -z "$OS_PASSWORD" ]]
 then
@@ -51,7 +51,7 @@ do
 done
 if [ "${MODE}" == "Join" ]
 then
-  read -p "Enter the peers Public IP (at least 3): " PEER
+  read -p "Enter the peers(at least 3) Public IPs: " PEER
   if [ "${PEER}" == "" ]; then echo "Peer cannot be empty"; exit 1; fi
 else
   TOKEN=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
@@ -74,9 +74,9 @@ if [ "${NAME}" == "" ]; then echo "Name cannot be empty"; exit 1; fi
 
 if [ "${MODE}" == "Create" ]
 then
-  heat stack-create -f stack-${OS_REGION_NAME}.yml -P os_username=$OS_USERNAME -P os_password=$OS_PASSWORD -P os_tenant=$OS_TENANT_NAME -P node_count=${NODE_COUNT} -P storage_count=${STORAGE_COUNT} -P keypair_name=${KEYPAIR} -P token=${TOKEN} -P ceph=1 -P monitoring=${MONITORING} ${NAME}
+  heat stack-create -f stack-${OS_REGION_NAME}.yml -P os_username=${OS_USERNAME} -P os_password=${OS_PASSWORD} -P os_auth_url=${OS_AUTH_URL} -P os_tenant_name=${OS_TENANT_NAME} -P node_count=${NODE_COUNT} -P storage_count=${STORAGE_COUNT} -P keypair_name=${KEYPAIR} -P token=${TOKEN} -P ceph=1 -P monitoring=${MONITORING} ${NAME}
 else
-  heat stack-create -f stack-${OS_REGION_NAME}.yml -P os_username=$OS_USERNAME -P os_password=$OS_PASSWORD -P os_tenant=$OS_TENANT_NAME -P node_count=${NODE_COUNT} -P storage_count=${STORAGE_COUNT} -P keypair_name=${KEYPAIR} -P token=${TOKEN} -P ceph=1 -P monitoring=${MONITORING} -P peer=${PEER} ${NAME}
+  heat stack-create -f stack-${OS_REGION_NAME}.yml -P os_username=${OS_USERNAME} -P os_password=${OS_PASSWORD} -P os_auth_url=${OS_AUTH_URL} -P os_tenant_name=${OS_TENANT_NAME} -P node_count=${NODE_COUNT} -P storage_count=${STORAGE_COUNT} -P keypair_name=${KEYPAIR} -P token=${TOKEN} -P ceph=1 -P monitoring=${MONITORING} -P peer="$PEER" ${NAME}
 fi
 
 until heat stack-show ${NAME} 2> /dev/null | egrep 'CREATE_COMPLETE|CREATE_FAILED'
